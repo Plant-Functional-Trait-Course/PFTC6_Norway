dd72d2cee043cff93ac688c8491839455ecc6558
audhalbritter <me@audhalbritter.com>
2022-06-28 06:53:37 +0200

    add threeD data collection

--- R/Rdatagathering/make_vcg_3D_species_lists.R
+++ R/Rdatagathering/make_vcg_3D_species_lists.R
@@ -16,7 +16,7 @@ library(tidyverse)
 #          path = "clean_data",
 #          remote_path = "3_Community_data")
 
-
+#### GET COMMUNITY DATA ####
 # vcg plant community data
 con <- dbConnect(SQLite(), dbname = "clean_data/seedclim.sqlite")
 
@@ -97,6 +97,7 @@ incline_community <- read_csv2(file = "clean_data/INCLINE_community_2018_2019_20
   summarise(cover = mean(cover))
 
 
+####  COMMON ALPINE AND GENERAL SPECIES FOR WHOLE GRADIENT AND ALL SITES ####
 community <- bind_rows(
   vcg = vcg_community,
   threeD = threeD_community,
@@ -144,6 +145,71 @@ community |>
 
   #writexl::write_xlsx(path = "vcg_threeD_specie_list.xlsx")
 
+
+
+
+#### PLOT LEVEL SPECIES LISTS ####
+
+
+vcg_plot_community <- tbl(con, "turf_community")  |>
+  select(-cf, -flag) |>
+  left_join(tbl(con, "turfs"), by = "turfID") |>
+
+  # only control plots
+  filter(TTtreat %in% c("TTC")) |>
+  select(-RTtreat, -GRtreat, -destinationPlotID) |>
+
+  # join plot, block and site IDs
+  left_join(tbl(con, "plots"), by = c("originPlotID" = "plotID")) |>
+  rename("plotID" = originPlotID) |>
+  select(-aspect, -slope) |>
+  left_join(tbl(con, "blocks"), by = c("blockID")) |>
+  select(-aspect, -slope) |>
+  left_join(tbl(con, "sites"), by = c("siteID")) |>
+  select(-comment, -norwegian_name, -site_code, -latitude, -longitude, -c(biogeographic_zone:precipitation_level)) |>
+
+  # filter 2 sites, blocks and last year
+  filter(siteID %in% c("Hogsete", "Vikesland"),
+         ### CHECK IF THESE ARE OK!!!
+         blockID %in% c("Hog1", "Hog2", "Hog3", "Vik3", "Vik4", "Vik5"),
+         year == 2019,
+         species != "NID.seedling") |>
+  left_join(tbl(con, "taxon"), by = "species") |>
+  select(siteID, blockID, turfID, species = species_name, cover) |>
+  mutate(warming = "ambient",
+         grazing = "C",
+         Namount_kg_ha_y = 0) |>
+  collect()
+
+# vcg_plot_community |>
+#   filter(siteID == "Vikesland") |>
+#   select(blockID, species, cover) |>
+#   pivot_wider(names_from = blockID, values_from = cover)
+
+
+NitrogenDictionary <- tibble(Nlevel = c(1,6,5,3,10,7,4,8,9,2),
+                             Namount_kg_ha_y = c(0, 5, 1, 0, 150, 10, 0.5, 50, 100, 0))
+
+threeD_plot_community <- read_csv("clean_data/THREE-D_Cover_2019-2021.csv") |>
+  left_join(NitrogenDictionary, by = "Nlevel") |>
+  filter(year == 2021 & grazing == "N" & Namount_kg_ha_y == 0 |
+           year == 2021 & grazing == "C" & Namount_kg_ha_y %in% c(0, 5, 10, 50, 150)
+         ) |>
+  mutate(destBlockID = as.character(destBlockID),
+         elevation = if_else(destSiteID == "Lia", 1290, 920)) |>
+  select(siteID = destSiteID, blockID = destBlockID, turfID, warming, grazing, Namount_kg_ha_y, species, cover)
+
+
+
+bind_rows(
+  vcg = vcg_plot_community,
+  threeD = threeD_plot_community,
+  .id = "experiment"
+) |>
+  group_by(turfID) |>
+  arrange(experiment, siteID, blockID, turfID, -cover) |>
+  mutate(cumsum = cumsum(cover)) |>
+  filter(cumsum <= 90) |> ungroup() |> count(experiment)
 
 
 
