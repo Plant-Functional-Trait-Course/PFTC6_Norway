eca5506df96da6ed1541fcb9cfa45fb71bfd6a05
the-esherman <emiltheemperor@gmail.com>
2022-12-09 16:58:38 +0100

    Cleaned code but kept function redundancy

--- R/traits/biomass_for_chemTraits.R
+++ R/traits/biomass_for_chemTraits.R
@@ -3,34 +3,84 @@
 # Load libraries
 library(tidyverse)
 library(readxl)
-library(googlesheets4)
+library(writexl)
+
+# Functions
+# total DW if combined within groups
+DWfilterTotal <- function(DataSet, logical){
+  DataSet %>%
+    filter(minimum_chemTraitDW == logical) %>%
+    group_by(project, siteID, experiment, taxon) %>%
+    summarise(sumDW = sum(dry_mass))
+}
+# Total count of DW's within groups
+DWfilterCount <- function(DataSet, logical){
+  DataSet %>%
+    filter(minimum_chemTraitDW == logical) %>%
+    group_by(project, siteID, experiment, taxon) %>%
+    summarise(amount = n())
+}
 
 # import data
-raw_dry_mass <- read_excel(path = "raw_data/traits/PFTC6_Norway_Leaf_traits_2022.xlsx", sheet = "DryMass")
+dry_mass <- read_excel(path = "clean_data/traits/PFTC6_Norway_traits_DW_clean_2022.xlsx")
 
-# From google sheets
-raw_dry_mass_google <- read_sheet("https://docs.google.com/spreadsheets/d/1ncqbniu0NUzCfcNe2fOl6M2Yj-BeOEXcYlPZHwNiIbk/edit#gid=0",
-                   sheet = "DryMass",
-                   col_names = TRUE)
+cleaned_traits <- read_excel(path = "clean_data/traits/PFTC6_Norway_traits_clean_2022.xlsx")
+
+
 # Minimum dry mass in g
 minDW = 0.03
 
-
 # Check each value if enough
-raw_dry_mass_min <- raw_dry_mass %>%
+dry_mass_min <- dry_mass %>%
   mutate(minimum_chemTraitDW = if_else(dry_mass >= minDW, T, F))
 
 
 # Read the cleaned data file or add this code snippet to the cleaning file after cleaning. For now just using the clean_trait_data.R script and having the values loaded and cleaned from there.
-clean_traits2_DW <- left_join(clean_traits2, raw_dry_mass_min, by = "ID")
+clean_traits_DW <- left_join(cleaned_traits, dry_mass_min, by = "ID")
 
 # Check if there is too little material if the sample can be merged with another sample from the same site, treatment, and plot
-clean_traits2_DW <- clean_traits2_DW %>%
-  mutate(merge = case_when())
+
+DW_small.1 <- clean_traits_DW %>%
+  filter(minimum_chemTraitDW==F) %>%
+  group_by(project, siteID, experiment, taxon) %>%
+  summarise(sumS = sum(dry_mass))
+DW_small.2 <- clean_traits_DW %>%
+  filter(minimum_chemTraitDW==F) %>%
+  group_by(project, siteID, experiment, taxon) %>%
+  summarise(amountS = n())
+DW_small <- left_join(DW_small.1,DW_small.2, by =c("project", "siteID", "experiment", "taxon"))
+DW_small <- DW_small %>%
+  add_column(merges_amount = DW_small$sumS / minDW) %>%
+  mutate(merges_amount = trunc(merges_amount))
+
+# Not needed.
+DW_big.1 <- clean_traits_DW %>%
+  filter(minimum_chemTraitDW==T) %>%
+  group_by(project, siteID, experiment, taxon) %>%
+  summarise(sumB = sum(dry_mass))
+DW_big.2 <- clean_traits_DW %>%
+  filter(minimum_chemTraitDW==T) %>%
+  group_by(project, siteID, experiment, taxon) %>%
+  summarise(amountB = n())
+DW_big <- left_join(DW_big.1,DW_big.2, by =c("project", "siteID", "experiment", "taxon"))
+
+DW_both <- left_join(DW_big,DW_small, by =c("project", "siteID", "experiment", "taxon"))
+DW_both <- DW_both %>%
+  mutate(total_wMerge = amountB + if_else(is.na(merges_amount), 0, merges_amount))
 
 
 
-# # Wet and dry mass do not make sense for these species
-# mutate(dry_mass_g = ifelse(genus %in% c("Baccharis", "Lycopodiella", "Lycopodium", "Hypericum"), NA_real_, dry_mass_g),
-#        wet_mass_g = ifelse(genus %in% c("Baccharis", "Lycopodiella", "Lycopodium", "Hypericum"), NA_real_, wet_mass_g),
-#        leaf_area_cm2 = ifelse(genus %in% c("Baccharis", "Lycopodiella", "Lycopodium", "Hypericum"), NA_real_, leaf_area_cm2)) |>
+DW_small.1_test <- DWfilterTotal(clean_traits_DW, F)
+DW_small.2_test <- DWfilterCount(clean_traits_DW, F)
+DW_big.1_test <- DWfilterTotal(clean_traits_DW, T)
+DW_big.2_test <- DWfilterCount(clean_traits_DW, T)
+DW_both_test <- left_join(DW_big.1_test, DW_big.2_test, by = c("project", "siteID", "experiment", "taxon")) %>%
+  left_join(.,DW_small.1_test, by = c("project", "siteID", "experiment", "taxon")) %>%
+  left_join(.,DW_small.2_test, by = c("project", "siteID", "experiment", "taxon")) %>%
+  rename("sumB" = sumDW.x,
+         "amountB" = amount.x,
+         "sumS" = sumDW.y,
+         "amountS" = amount.y) %>%
+  add_column(merges_amount = .$sumS / minDW) %>%
+  mutate(merges_amount = trunc(merges_amount)) %>%
+  mutate(total_wMerge = amountB + if_else(is.na(merges_amount), 0, merges_amount))
